fb9353c3d97cfd44411e36fe8e5bf8eaccba3a9a
muzaisimao <muzaisimao@163.com>
2019-07-08 23:36:37 +0800

    多实例版, 基本可用

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-// egg-mysql/app.js
-module.exports = app => {
-    // 第一个参数 mysql 指定了挂载到 app 上的字段，我们可以通过 `app.mysql` 访问到 MySQL singleton 实例
-    // 第二个参数 createMysql 接受两个参数(config, app)，并返回一个 MySQL 的实例
-    app.addSingleton('cxios', createCxios);
-}
+// egg-cxios/app.js
+const cxios = require('./lib/cxios.js');
 
-function createCxios(config, app) {
-    
-}
+module.exports = app => { cxios(app) }
